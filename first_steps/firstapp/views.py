a662e28fa2136269f44d099646877b090aadf5e5
MyKasero <mykasero20@gmail.com>
2024-07-24 19:19:14 +0200

    full lesson 5

--- first_steps/firstapp/views.py
+++ first_steps/firstapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse lesson <4
+from .models import Tour
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("First App")
+    # return HttpResponse("First App") lesson <4
+    tours = Tour.objects.all()
+    context = {'tours' : tours}
+    return render(request, "tours/index.html", context)
