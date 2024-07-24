002cd3186e0232742dcac66a1411da5bb73f214b
MyKasero <mykasero20@gmail.com>
2024-07-24 18:28:33 +0200

    small changes lesson 3

--- first_steps/firstapp/models.py
+++ first_steps/firstapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    '''
+        - origin country
+        - destination
+        - number of nights
+        - price for the tour
+    '''
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
