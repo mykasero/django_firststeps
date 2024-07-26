355fe5d81f25b65a79309a221e68a8246043afb8
MyKasero <mykasero20@gmail.com>
2024-07-26 23:39:40 +0200

    lesson 7

--- first_steps/firstapp/urls.py
+++ first_steps/firstapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index
-
+# from .views import index #lesson <=6
+from . import views
 # Define a list of url patterns
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', views.home, name="homepage"),
+    path('contact/', views.contact_view, name = 'contact_view'),
+    path('contact/', views.success_view, name = 'contact-success'),
 ]
 
--- first_steps/firstapp/views.py
+++ first_steps/firstapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect # redirect lesson 7
 # from django.http import HttpResponse lesson <4
-from .models import Tour
+# from .models import Tour lesson 4-7
+from .forms import ContactForm #lesson 7
 
 # Create your views here.
 
@@ -11,6 +12,32 @@ from .models import Tour
 #     context = {'tours' : tours}
 #     return render(request, "tours/index.html", context)
 
-#lesson >6
-def index(request):
-    return render(request, 'tours/index.html')
+#lesson 7
+# def index(request):
+#     return render(request, 'tours/index.html')
+
+#lesson 8
+#home page view function
+def home_view(request):
+    return render(request, 'tours/home.html')
+
+#contact form handling function
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+
+    context = {'form' : form}
+    return render(request, 'tours/contact.html', context)
+
+#success page
+
+def success_view(request):
+    return render(request, 'tours/success.html')
+
+
+
