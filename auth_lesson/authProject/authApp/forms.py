d0bb3d42ac3d2104902c80a61ddce023e98379f8
MyKasero <mykasero20@gmail.com>
2024-07-29 21:57:35 +0200

    auth lesson

--- auth_lesson/authProject/authApp/forms.py
+++ auth_lesson/authProject/authApp/forms.py
@@ -1,3 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
 
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(max_length=20,
+                               widget = forms.PasswordInput)
+    password_confirm = forms.CharField(max_length=20,
+                               widget = forms.PasswordInput,
+                                label = "Confirm password")
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "password",
+            "password_confirm"
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        #check if the passwords match
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords don't match!!!")
+
+        return cleaned_data
--- auth_lesson/authProject/authApp/views.py
+++ auth_lesson/authProject/authApp/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.models import User
+from .forms import RegisterForm
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = User.objects.create_user(username = username,
+                                     password = password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = RegisterForm()
+            return render(request, 'accounts/register.html', {'form':form})
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request,
+                            username = username,
+                            password = password)
+        if user is not None:
+            login(request, user)
+            next_url = request.POST.get("next") or request.GET.get("next") or 'home'
+            return redirect(next_url)
+        else:
+            error_message = "Invalid Credentials!"
+
+    return render(request, 'accounts/login.html', {'error': error_message})
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    else:
+        return redirect('home')
+
+#Home View
+#Using the decorator
+
+@login_required
+def home_view(request):
+    pass
+
