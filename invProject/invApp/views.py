069a4d4e6a0237145eb459c947e2a9a82b5671cf
MyKasero <mykasero20@gmail.com>
2024-08-02 23:05:23 +0200

    general structure

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import ProductForm
+from .models import Product
+
+# CRUD
+
+# Home View
+
+def home_view(request):
+    return render(request, )
+
+# Create View
+
+
+# Read View
+
+
+# Update View
+
+
+# Delete View
+
